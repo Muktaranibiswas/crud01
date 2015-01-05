845560eb179da73991ac25b7c69c5bbe213d450b
saimasarifa <saimasarifa@gmail.com>
2015-01-05 15:58:43 +0600

    basic php language

---
+++ basic php/math.php
@@ -0,0 +1,103 @@
+<?php
+/*$name='your name';
+for ($i=1; $i<=5; $i++)
+{
+  echo "hello $name! <br/>";
+
+}*/
+
+
+
+/*echo "Hello world".'<br/>';
+
+
+echo "This is a message from mukta"*/
+
+
+
+/*$number1="100";
+$number2=12;
+$word='hello';
+$word2='hi';
+
+
+echo $number1 + $word;
+echo"<br />";
+echo $number1 + $number2;
+echo"<br />";
+echo $word + $word2;*/
+
+
+/*
+$x= (7+3)*6;
+echo $x;
+echo"<br />";
+$y =100/(4*5);
+echo $y;
+echo"<br />";
+
+$z=7+(5*(8/2)+(4+6));
+echo $z."\n";
+//echo"<br />";
+$c=1%3;
+echo $c."\n";
+$c=3%1;
+echo $c;*/
+
+//echo__line__;
+
+
+
+/*output:
+$var = array(
+    2=>1,
+    3=>3,
+    "b"=4,
+    4=2,
+);*/
+
+
+
+/*$words_in_sentence = array(
+    0 => "program",
+    1 => "in",
+    2 => "PHP",
+    3 => "is",
+    4 => "Fun"
+
+);
+
+print_r ($words_in_sentence[4]);*/
+
+
+
+
+
+/*$my_test_array = array(
+    2=>1,
+    3=>3,
+    "b"=>4,
+    4=>2
+
+);
+$my_test_array[4]=8;
+$my_test_array[5]="mukta";
+$my_test_array[3]=50;
+
+echo $my_test_array[3];
+//var_dump($my_test_array);*/
+
+
+/*define ('TEST','This is a test!');
+echo TEST . PHP_EOL;
+echo Test . PHP_EOL;*/
+
+
+/*$fruit = array("grapes","apple","cherry");
+echo $fruit[2][3];*/
+
+$arr = array ("data" => "USA", 12 =>true);
+echo $arr["data"];
+echo $arr[12];
+
+?>
---
+++ basic php/quotes.php
@@ -0,0 +1,16 @@
+<?php
+$a=1;
+//echo $a;
+//echo'$a';
+//echo 'who said: "I\'ll be back"?';
+//echo "$a";
+//echo "hello\$a how are you";
+//echo '$a how are you';
+//$link = "<a href= 'http://mycompany.com'>Example</a>";
+
+//$school1="daulatkhan govt girl's high school";
+
+$school2='daulatkhan govt girl\'s high school';
+//echo "school1";
+echo 'school2';
+
